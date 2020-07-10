b15d6e3764abea2a1b45f09a1b1d57f8d4a8e899
coderoo7 <shubhamyadav.70007@gmail.com>
2020-07-11 00:12:14 +0530

    Solved calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,47 @@
-function add () {
-	
+function add (a,b) {
+	return a+b;
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	return a-b;
 }
 
-function sum () {
+function sum (arr) {
+	if(arr.length === 0){
+		return 0;
+	}
 	
+	let sum = 0;
+	arr.forEach(num => sum += num);
+	return sum;
 }
 
-function multiply () {
+function multiply (arr) {
+	if(arr.length === 0) {
+		return 0;
+	}
 	
+	let product = 1;
+	arr.forEach(num => product *= num);
+	return product;
 }
 
-function power() {
+function power(num,pow) {
+	if(pow === 0)
+		return 1;
 	
+	let result = 1;
+	for(let i=0; i<pow; i++){
+		result *= num;
+	}
+	return result;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if(num===1 || num === 0)
+		return 1;
+	else
+		return num * factorial(num-1);
 }
 
 module.exports = {
