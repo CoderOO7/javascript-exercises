95d6e27c82f4641f1d938baedf2048d1701bff9c
coderoo7 <shubhamyadav.70007@gmail.com>
2020-07-11 23:54:00 +0530

    Solved Palindrome Problem

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    const parsedString = parseAlphabetsFromSting(string);
+    console.log({parsedString})
+    const size = parsedString.length;
+    for(let i = 0; i < size; i++){
+        if(parsedString[i] !== parsedString[size-i-1])
+            return false;
+    }
+    return true;
 }
+
+const parseAlphabetsFromSting = (string) => string.match(/[A-z]+/g).join('').toLowerCase();
 
 module.exports = palindromes
